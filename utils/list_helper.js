1e2663d4fbb1989519869022504cc886dda62a95
chris <christau90s@gmail.com>
2021-02-08 11:36:35 +0200

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,20 @@
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return "no blogs"
+    }
+
+    const fav = blogs.sort((b1, b2) => b2.likes - b1.likes)[0]
+
+    return {
+        title: fav.title,
+        author: fav.author,
+        likes: fav.likes,
+    }
+}
+
 module.exports = {
     totalLikes,
+    favoriteBlog,
 }
