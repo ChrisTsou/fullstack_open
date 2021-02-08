229b98901018c7bafb3f55270e3be17840fbfeee
chris <christau90s@gmail.com>
2021-02-08 11:11:15 +0200

    4.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,18 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+        jest: true,
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 12,
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "never"],
+    },
 }
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,5 @@
-const dummy = (blogs) => {
-    return 1
-}
+const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
 module.exports = {
-    dummy,
+    totalLikes,
 }
