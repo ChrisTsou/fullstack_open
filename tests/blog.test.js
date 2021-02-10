c14ee7caeee5ed3b2d8cd6d4caf5fe87172f9c4f
chris <christau90s@gmail.com>
2021-02-10 15:28:24 +0200

    4.9

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -19,4 +19,12 @@ test("right number of blogs are returned", async () => {
     expect(response.body.length).toBe(testHelper.initialBlogs.length)
 })
 
+test("blogs have id property", async () => {
+    const response = await api.get("/api/blogs")
+
+    response.body.forEach((blog) => {
+        expect(blog.id).toBeDefined()
+    })
+})
+
 afterAll(() => mongoose.connection.close())
