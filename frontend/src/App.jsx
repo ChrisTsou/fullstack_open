bd13cedaf8fd67552580122acfaf85e003221948
chris <christau90s@gmail.com>
2021-02-19 12:21:45 +0200

    5.5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,157 +1,109 @@
-import React, { useState, useEffect } from "react"
-import Blog from "./components/Blog"
-import Notification from "./components/Notification"
-import blogService from "./services/blogs"
-import loginService from "./services/login"
+import React, { useState, useEffect, useRef } from 'react';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
+import LoginForm from './components/LoginForm';
+import Togglable from './components/Togglable';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import BlogForm from './components/BlogForm';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  // const [errorMessage, setErrorMessage] = useState(null)
-  const [user, setUser] = useState(null)
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [blogTitle, setBlogTitle] = useState("")
-  const [blogAuthor, setBlogAuthor] = useState("")
-  const [blogUrl, setBlogUrl] = useState("")
-  const [notification, setNotification] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs))
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser")
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
+  }, []);
 
   const notificationWithTimeout = (message, isError = false) => {
-    setNotification({ message, isError })
+    setNotification({ message, isError });
     setTimeout(() => {
-      setNotification(null)
-    }, 5000)
-  }
+      setNotification(null);
+    }, 5000);
+  };
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     try {
-      const user = await loginService.login({ username, password })
-      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user))
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername("")
-      setPassword("")
-      notificationWithTimeout("logged in")
+      const user = await loginService.login({ username, password });
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+      notificationWithTimeout('logged in');
     } catch (exception) {
-      notificationWithTimeout(exception.message, true)
+      notificationWithTimeout(exception.message, true);
     }
-  }
+  };
 
   const handleLogout = () => {
-    window.localStorage.clear()
-    setUser(null)
-    notificationWithTimeout("logged out")
-  }
+    window.localStorage.clear();
+    setUser(null);
+    notificationWithTimeout('logged out');
+  };
 
-  const handleCreateBlog = async (event) => {
-    event.preventDefault()
+  const blogFormRef = useRef();
 
+  const addBlog = async (blogObject) => {
     try {
-      const newBlog = await blogService.create({
-        title: blogTitle,
-        author: blogAuthor,
-        url: blogUrl,
-      })
+      blogFormRef.current.toggleVisibility();
+      await blogService.create(blogObject);
 
-      setBlogs(blogs.concat(newBlog))
-      notificationWithTimeout(
-        `a new blog ${newBlog.title} by ${newBlog.author} added`
-      )
+      setBlogs(blogs.concat(blogObject));
+      notificationWithTimeout(`a new blog ${blogObject.title} by ${blogObject.author} added`);
     } catch (exception) {
-      notificationWithTimeout(exception.message, true)
+      notificationWithTimeout(exception.message, true);
     }
-  }
+  };
 
-  const loginForm = () => (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
-  )
-
-  const blogsRender = () => (
+  const loggedInRender = () => (
     <div>
       <h2>blogs</h2>
       <p>
-        {user.name} logged in <button onClick={handleLogout}>logout</button>
+        {user.name}
+        {' '}
+        logged in
+        <button onClick={handleLogout}>logout</button>
       </p>
-      <h2>create new</h2>
-      <form onSubmit={handleCreateBlog}>
-        <div>
-          Title:
-          <input
-            type="text"
-            value={blogTitle}
-            name="BlogTitle"
-            onChange={({ target }) => setBlogTitle(target.value)}
-          />
-        </div>
-        <div>
-          Author:
-          <input
-            type="text"
-            value={blogAuthor}
-            name="BlogAuthor"
-            onChange={({ target }) => setBlogAuthor(target.value)}
-          />
-        </div>
-        <div>
-          Url:
-          <input
-            type="text"
-            value={blogUrl}
-            name="BlogUrl"
-            onChange={({ target }) => setBlogUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <Togglable buttonLabel="new note" ref={blogFormRef}>
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
     </div>
-  )
+  );
 
   return (
     <div>
       <Notification notification={notification} />
-      {user ? blogsRender() : loginForm()}
+      {user ? (
+        loggedInRender()
+      ) : (
+        <LoginForm
+          handleSubmit={handleLogin}
+          handleUsernameChange={({ target }) => setUsername(target.value)}
+          handlePasswordChange={({ target }) => setPassword(target.value)}
+          username={username}
+          password={password}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/BlogForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+const BlogForm = ({ createBlog }) => {
+  const [blogTitle, setBlogTitle] = useState('');
+  const [blogAuthor, setBlogAuthor] = useState('');
+  const [blogUrl, setBlogUrl] = useState('');
+
+  const addBlog = (event) => {
+    event.preventDefault();
+
+    createBlog({
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogUrl,
+    });
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          Title:
+          <input
+            type="text"
+            value={blogTitle}
+            name="BlogTitle"
+            onChange={({ target }) => setBlogTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={blogAuthor}
+            name="BlogAuthor"
+            onChange={({ target }) => setBlogAuthor(target.value)}
+          />
+        </div>
+        <div>
+          Url:
+          <input
+            type="text"
+            value={blogUrl}
+            name="BlogUrl"
+            onChange={({ target }) => setBlogUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
