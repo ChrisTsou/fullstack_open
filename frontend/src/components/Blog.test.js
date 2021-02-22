f7683e9aa623a19b5bfc8aa20a806b1a809e7f7b
chris <christau90s@gmail.com>
2021-02-22 13:13:56 +0200

    5.14

--- frontend/src/components/Blog.test.js
+++ frontend/src/components/Blog.test.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, prettyDOM, render } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title,author and not url,likes', () => {
-  const blog = {
-    title: 'test-title',
-    author: 'test-author',
-    url: 'test-url',
-    likes: 3,
-    user: {
-      username: 'test-username',
-      name: 'test-name',
-    },
-  }
+const blog = {
+  title: 'test-title',
+  author: 'test-author',
+  url: 'test-url',
+  likes: 3,
+  user: {
+    username: 'test-username',
+    name: 'test-name',
+  },
+}
 
-  const component = render(
+let component
+
+beforeEach(() => {
+  component = render(
     <Blog
       blog={blog}
       currentUser={{
@@ -26,9 +28,22 @@ test('renders title,author and not url,likes', () => {
       deleteBlog={() => null}
     />
   )
+})
+
+test('renders title,author and not url,likes', () => {
   const info = component.container.querySelector('.blogInfo')
   const details = component.container.querySelector('.blogDetails')
 
   expect(info).toHaveTextContent('test-title test-author')
   expect(details).toBeNull()
+})
+
+test('renders url,likes when button is pressed', () => {
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const details = component.container.querySelector('.blogDetails')
+  expect(details).toHaveTextContent('test-url')
+  expect(details).toHaveTextContent('likes: 3')
+  expect(details).toHaveTextContent('test-name')
 })
