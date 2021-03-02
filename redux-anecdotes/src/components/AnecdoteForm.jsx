d0c800d6cce43c9b1733fa29b960ec21da931379
ChrisT <christau90s@gmail.com>
2021-03-02 14:10:13 +0200

    6.8

--- redux-anecdotes/src/components/AnecdoteForm.jsx
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import { createAnecdote } from "../reducers/anecdoteReducer";
-import { useDispatch } from "react-redux";
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const addAnecdote = (event) => {
-    event.preventDefault();
-    const content = event.target.anecdote.value;
-    event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
-  };
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
 
   return (
-    <form onSubmit={addAnecdote}>
-      <div>
-        <input name="anecdote" />
-      </div>
-      <button type="submit">create</button>
-    </form>
-  );
-};
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
 
-export default AnecdoteForm;
+export default AnecdoteForm
--- redux-anecdotes/src/reducers/anecdoteReducer.test.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.test.js
@@ -1,50 +1,50 @@
-import deepFreeze from "deep-freeze";
-import anecdoteReducer from "./anecdoteReducer";
+import deepFreeze from 'deep-freeze'
+import anecdoteReducer from './anecdoteReducer'
 
-describe("anecdoteReducer", () => {
+describe('anecdoteReducer', () => {
   const initialState = [
     {
-      content: "anecdote0",
+      content: 'anecdote0',
       id: 0,
       votes: 0,
     },
     {
-      content: "anecdote1",
+      content: 'anecdote1',
       id: 1,
       votes: 0,
     },
-  ];
+  ]
 
-  test("vote is incremented", () => {
+  test('vote is incremented', () => {
     const action = {
-      type: "VOTE",
+      type: 'VOTE',
       data: 1,
-    };
-    const state = initialState;
+    }
+    const state = initialState
 
-    deepFreeze(state);
-    const newState = anecdoteReducer(state, action);
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
     expect(newState).toEqual([
       state[0],
       {
-        content: "anecdote1",
+        content: 'anecdote1',
         id: 1,
         votes: 1,
       },
-    ]);
-  });
+    ])
+  })
 
-  test("anecdote is created", () => {
+  test('anecdote is created', () => {
     const action = {
-      type: "CREATE",
-      data: "anecdote2",
-    };
-    const state = initialState;
+      type: 'CREATE',
+      data: 'anecdote2',
+    }
+    const state = initialState
 
-    deepFreeze(state);
-    const newState = anecdoteReducer(state, action);
-    expect(newState[2].content).toEqual("anecdote2");
-    expect(newState[2].id).toBeDefined();
-    expect(newState[2].votes).toEqual(0);
-  });
-});
+    deepFreeze(state)
+    const newState = anecdoteReducer(state, action)
+    expect(newState[2].content).toEqual('anecdote2')
+    expect(newState[2].id).toBeDefined()
+    expect(newState[2].votes).toEqual(0)
+  })
+})
