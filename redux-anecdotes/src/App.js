51d7d8eee2ea292985d5697161a34bea6be3ed48
ChrisT <christau90s@gmail.com>
2021-03-02 13:08:38 +0200

    6.5

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -16,17 +16,19 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(anecdoteVote(anecdote.id))}>
-              vote
-            </button>
+      {anecdotes
+        .sort((a0, a1) => a1.votes - a0.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => dispatch(anecdoteVote(anecdote.id))}>
+                vote
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div>
