4bc4570610e14ae53a139bbc10fb0344cc1b0573
ChrisT <christau90s@gmail.com>
2021-03-04 12:33:53 +0200

    6.14

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => (
-  <div>
-    <Notification />
-    <h2>Anecdotes</h2>
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)))
+  })
+
+  return (
+    <div>
+      <Notification />
+      <h2>Anecdotes</h2>
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
 
 export default App
