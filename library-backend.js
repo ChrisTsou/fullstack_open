c578c4cc91869bcd67935ae9d47fb651b814e812
ChrisT <christau90s@gmail.com>
2021-03-28 14:42:20 +0300

    8.4

--- library-backend.js
+++ library-backend.js
@@ -102,22 +102,29 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book]!
+    allBooks(author: String): [Book]!
     allAuthors: [Author]!
   }
 `;
+
+const filterBooksByAuthor = (author) =>
+  books.filter((book) => author === book.author);
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return filterBooksByAuthor(args.author);
+      }
+      return books;
+    },
     allAuthors: () => authors,
   },
 
   Author: {
-    bookCount: (root) =>
-      books.filter((book) => root.name === book.author).length,
+    bookCount: (root) => filterBooksByAuthor(root.name).length,
   },
 };
 
