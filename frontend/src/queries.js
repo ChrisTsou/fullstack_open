41bffd2d9b9ebee2e2f5fee5f5b3978d29926a79
ChrisT <christau90s@gmail.com>
2021-03-29 15:53:14 +0300

    8.9

--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -10,3 +10,14 @@ export const ALL_AUTHORS = gql`
     }
   }
 `;
+
+export const ALL_BOOKS_NO_GENRE = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+    }
+  }
+`;
