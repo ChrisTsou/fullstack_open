e005be9ebb2dca50bc457119fcccea4ec7904f9c
ChrisT <christau90s@gmail.com>
2021-03-30 14:57:41 +0300

    8.14

--- backend/library-backend.js
+++ backend/library-backend.js
@@ -1,96 +1,31 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { v1: uuid } = require("uuid");
+const mongoose = require("mongoose");
+const Book = require("./schemas/Book");
+const Author = require("./schemas/Author");
 
-let authors = [
-  {
-    name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
-  },
-  {
-    name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
-  },
-  {
-    name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-];
+const MONGODB_URI =
+  "mongodb+srv://fullstackopen:christsou@cluster0.mocqg.mongodb.net/libraryApp?retryWrites=true&w=majority";
+console.log("connecting to", MONGODB_URI);
 
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- */
-
-let books = [
-  {
-    title: "Clean Code",
-    published: 2008,
-    author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Agile software development",
-    published: 2002,
-    author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ["agile", "patterns", "design"],
-  },
-  {
-    title: "Refactoring, edition 2",
-    published: 2018,
-    author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring"],
-  },
-  {
-    title: "Refactoring to patterns",
-    published: 2008,
-    author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "patterns"],
-  },
-  {
-    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
-    author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ["refactoring", "design"],
-  },
-  {
-    title: "Crime and punishment",
-    published: 1866,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "crime"],
-  },
-  {
-    title: "The Demon ",
-    published: 1872,
-    author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ["classic", "revolution"],
-  },
-];
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) =>
+    console.log("failed to connect to MongoDB: ", error.message)
+  );
 
 const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
+    author: Author!
     genres: [String]!
+    id: ID!
   }
 
   type Author {
@@ -119,29 +54,41 @@ const typeDefs = gql`
   }
 `;
 
-const filterBooksByAuthor = (author) =>
-  books.filter((book) => author === book.author);
+const getModelCount = async (model) => {
+  const count = await model.countDocuments({});
+  return count;
+};
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) =>
-      books.filter(
-        (book) =>
-          (!args.author || args.author === book.author) &&
-          (!args.genre || book.genres.includes(args.genre))
-      ),
-    allAuthors: () => authors,
+    bookCount: () => getModelCount(Book),
+    authorCount: () => getModelCount(Author),
+    allBooks: async (root, args) => {
+      const genre = args.genre;
+      if (genre) {
+        const books = await Book.find({
+          genres: { $elemMatch: { $eq: genre } },
+        });
+        return books;
+      } else {
+        const books = await Book.find({});
+        return books;
+      }
+    },
+    allAuthors: async () => {
+      const authors = await Author.find({});
+      return authors;
+    },
   },
 
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() };
-      if (!authors.includes((author) => author.name === book.author)) {
-        authors = authors.concat({ name: book.author, born: null, id: uuid() });
-      }
-      books = books.concat(book);
+    addBook: async (root, { author, ...args }) => {
+      const book = new Book({
+        ...args,
+        author: new Author({ name: "testAuthor" }),
+      });
+      await book.save();
+
       return book;
     },
 
---
+++ backend/populateDb.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+const Book = require("./schemas/Book");
+const Author = require("./schemas/Author");
+
+const MONGODB_URI =
+  "mongodb+srv://fullstackopen:christsou@cluster0.mocqg.mongodb.net/libraryApp?retryWrites=true&w=majority";
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("connected to MongoDB"))
+  .catch((error) =>
+    console.log("failed to connect to MongoDB: ", error.message)
+  );
+
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+];
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "Robert Martin",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "Robert Martin",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "Martin Fowler",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "Joshua Kerievsky",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "Sandi Metz",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "The Demon ",
+    published: 1872,
+    author: "Fyodor Dostoevsky",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+];
+(async () => {
+  const authorsToInsert = authors.map(
+    ({ name, born }) => new Author({ name, born })
+  );
+  await Author.insertMany(authorsToInsert);
+
+  const booksToInsert = books.map(({ id, author: authorName, ...rest }) => {
+    const author = authorsToInsert.find((a) => a.name === authorName);
+    return new Book({ ...rest, author: author._id });
+  });
+  // await Book.insertMany(booksToInsert);
+
+  mongoose.disconnect().then(() => console.log("disconnected from MongoDB"));
+})();
