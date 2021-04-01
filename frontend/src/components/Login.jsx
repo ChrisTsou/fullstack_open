b1a0abf11e3b51aaa0fce999efa703a8d3a85e9e
ChrisT <christau90s@gmail.com>
2021-04-01 19:08:33 +0300

    8.21

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { useMutation } from "@apollo/client";
+import { useApolloClient, useMutation } from "@apollo/client";
 import { LOGIN } from "../mutations";
+import { ME } from "../queries";
 
 const Login = ({ show, setToken }) => {
+  const client = useApolloClient();
+  const [login, result] = useMutation(LOGIN);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  const [login, result] = useMutation(LOGIN);
 
   useEffect(() => {
     if (result.data) {
       const token = result.data.login.value;
       setToken(token);
       localStorage.setItem("user-token", token);
+      client.query({
+        query: ME,
+        fetchPolicy: "network-only",
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [result.data]);
--- frontend/src/components/Recommend.jsx
+++ frontend/src/components/Recommend.jsx
@@ -1,20 +1,31 @@
-import { useQuery } from "@apollo/client";
-import React from "react";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
 import { ALL_BOOKS, ME } from "../queries";
 import BookTable from "./BookTable";
 
 const Recommend = ({ show }) => {
   const meResult = useQuery(ME);
-  const booksResult = useQuery(ALL_BOOKS);
+  const [getBooks, booksResult] = useLazyQuery(ALL_BOOKS);
+  const [books, setBooks] = useState(null);
 
-  if (!show || meResult.loading || booksResult.loading) {
+  useEffect(() => {
+    if (meResult.data && meResult.data.me) {
+      getBooks({ variables: { genre: meResult.data.me.favoriteGenre } });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [meResult.data]);
+
+  useEffect(() => {
+    if (booksResult.data) {
+      setBooks(booksResult.data.allBooks);
+    }
+  }, [booksResult.data]);
+
+  if (!show || !meResult.data.me || !books) {
     return null;
   }
 
   const favoriteGenre = meResult.data.me.favoriteGenre;
-  const books = booksResult.data.allBooks.filter((b) =>
-    b.genres.includes(favoriteGenre)
-  );
 
   return (
     <>
