8277865ca75e268218312d7db1960e563bc49f7c
ChrisT <christau90s@gmail.com>
2021-04-24 14:39:56 +0300

    9.2

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,23 +1,30 @@
 function calculateBmi(height: number, weight: number): String {
-    const bmi = weight / Math.pow((height / 100), 2)
+  const bmi = weight / (height / 100) ** 2;
 
-    if (bmi > 40) {
-        return 'Obese Class III (Very severely obese)';
-    } else if (bmi > 35) {
-        return 'Obese Class II (Severely obese)';
-    } else if (bmi > 30) {
-        return 'Obese Class I (Moderately obese)';
-    } else if (bmi > 25) {
-       return 'Overweight';
-    } else if (bmi > 18.5) {
-        return 'Normal (healthy weight)'
-    } else if (bmi > 16) {
-        return 'Underweight'
-    } else if (bmi > 15) {
-        return 'Severely underweight'
-    } else if (bmi <= 15) {
-        return 'Very Severely underweight'
-    }
+  if (bmi > 40) {
+    return 'Obese Class III (Very severely obese)';
+  }
+  if (bmi > 35) {
+    return 'Obese Class II (Severely obese)';
+  }
+  if (bmi > 30) {
+    return 'Obese Class I (Moderately obese)';
+  }
+  if (bmi > 25) {
+    return 'Overweight';
+  }
+  if (bmi > 18.5) {
+    return 'Normal (healthy weight)';
+  }
+  if (bmi > 16) {
+    return 'Underweight';
+  }
+  if (bmi > 15) {
+    return 'Severely underweight';
+  }
+  if (bmi <= 15) {
+    return 'Very Severely underweight';
+  }
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 74));
---
+++ exerciseCalculator.ts
@@ -0,0 +1,58 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+function calculateExercises(hoursPerDay: Array<number>, target: number): ExerciseResult {
+  const periodLength = hoursPerDay.length;
+  const trainingDays = hoursPerDay.filter((n) => n !== 0).length;
+  const average = hoursPerDay.reduce((acc, v) => acc + v, 0) / periodLength;
+
+  const targetDiff = target - average;
+  const ratings = [
+    {
+      rating: 3,
+      ratingDescription: 'excellent!',
+    },
+    {
+      rating: 2,
+      ratingDescription: 'good job, push a bit harder',
+    },
+    {
+      rating: 1,
+      ratingDescription: 'exercise more',
+    },
+  ];
+
+  let rating;
+  let ratingDescription;
+  if (targetDiff > 0) {
+    if (targetDiff < 0.5) {
+      rating = ratings[1].rating;
+      ratingDescription = ratings[1].ratingDescription;
+    } else {
+      rating = ratings[2].rating;
+      ratingDescription = ratings[2].ratingDescription;
+    }
+  } else {
+    rating = ratings[0].rating;
+    ratingDescription = ratings[0].ratingDescription;
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    average,
+    target,
+    rating,
+    ratingDescription,
+    success: targetDiff <= 0,
+  };
+}
+
+console.log(calculateExercises([3, 0, 2, 0, 0, 3, 1], 2));
