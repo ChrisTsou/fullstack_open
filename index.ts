fecf345e393314dcdcec196d1cae1b637e6524cd
ChrisT <christau90s@gmail.com>
2021-04-27 16:17:35 +0300

    9.6

--- index.ts
+++ index.ts
@@ -1,23 +1,27 @@
-import express from 'express';
-import bmiCalculator from './bmiCalculator';
+import express from "express";
+import bmiCalculator from "./bmiCalculator";
 
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
 });
 
-app.get('/bmi', (req, res) => {
+app.get("/bmi", (req, res) => {
   const { height, weight } = req.query;
 
-  if (typeof height !== 'string' || typeof weight !== 'string') {
-    res.json({ error: 'malformatted parameters' });
+  if (typeof height !== "string" || typeof weight !== "string") {
+    res.json({ error: "malformatted parameters" });
   }
 
   try {
     res.json(bmiCalculator(String(height), String(weight)));
   } catch (e) {
-    res.json({ error: e.message });
+    if (e instanceof Error) {
+      res.json({ error: e.message });
+    } else {
+      throw new Error("unknown error type");
+    }
   }
 });
 
