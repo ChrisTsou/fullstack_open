32019ea5e3e67538ebd8362d6c2027f254858dad
ChrisT <christau90s@gmail.com>
2021-05-06 14:12:31 +0300

    9.10

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
-const app = express();
-app.use(express.json());
+import diagnoseService from "./services/diagnoseService";
 
+const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = 3001;
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.get("/api/diagnoses", (_req, res) => {
+  res.send(diagnoseService.getDiagnoses());
 });
 
 app.listen(PORT, () => {
