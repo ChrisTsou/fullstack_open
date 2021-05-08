516e3b2df74e080ede6a3ccd0070e5557474d326
ChrisT <christau90s@gmail.com>
2021-05-08 11:47:49 +0300

    9.13

--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,8 +1,13 @@
 import patientData from "../../data/patients.json";
 import { NewPatient, NonSensitivePatient, Patient } from "../types";
 import { v1 as uuid } from "uuid";
+import toNewPatient from "../utils";
 
-const patients: Array<Patient> = patientData;
+const patients: Array<Patient> = patientData.map((obj) => {
+  const object = toNewPatient(obj) as Patient;
+  object.id = obj.id;
+  return object;
+});
 
 const getNonSensitivePatients = (): Array<NonSensitivePatient> => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
