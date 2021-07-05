6688e1987e32e6ddf3be9d47611dfd0cd3be1050
ChrisT <christau90s@gmail.com>
2021-07-05 12:12:45 +0300

    10.13

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,17 +1,27 @@
 import { Formik } from "formik";
 import React from "react";
 import * as yup from "yup";
+import useSignIn from "../hooks/useSignIn";
 
 import SignInForm from "./SignInForm";
 
 const SignIn = () => {
+  const [signIn] = useSignIn();
+
   const initialValues = {
     username: "",
     password: "",
   };
 
-  const onSubmit = (values) => {
-    console.log(values);
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const validationSchema = yup.object().shape({
